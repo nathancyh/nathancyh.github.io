ed25c3121350037e2801c90597108fcbd0252649
nathan19952008@gmail.com <nathan19952008@gmail.com>
2021-01-25 00:33:58 +0800

    Commit 7

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 "use strict";
 const nav = document.getElementById("navi");
 const btnSwit = document.getElementById("btnSwitch");
-
+/////////////////////////////SCROLL REVEAL
+ScrollReveal().reveal(".about", { delay: 250 });
+ScrollReveal().reveal(".skillrow1", { delay: 250 });
+ScrollReveal().reveal(".work_box", { delay: 250 });
+ScrollReveal().reveal("#contact", { delay: 200 });
+//////////////////////////////NAV SWITCHER
 let switcher = function () {
   if (nav.classList.contains("navbar-dark")) {
     nav.classList.remove("navbar-dark");
@@ -18,19 +23,17 @@ let switcher = function () {
 
 btnSwit.addEventListener("click", switcher);
 
-//////////////////////////////
-// STICKY NAV
+//////////////////////////////// STICKY NAV
 const header = document.getElementById("home");
 const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) {
-    nav.classList.remove("sticky");
+    nav.classList.remove("hidden");
   } else {
-    nav.classList.add("sticky");
+    nav.classList.add("hidden");
   }
 };
 
@@ -41,3 +44,39 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+/////////////////////////////////TYPEWRITER
+const typewriter = document.querySelector(".typewriter");
+const aboutcontent = document.querySelector(".about-content");
+
+typewriter.addEventListener("animationend", function (e) {
+  if (e.animationName === "typing") {
+    aboutcontent.classList.remove("hidden");
+    aboutcontent.classList.add("animate__animated");
+    aboutcontent.classList.add("animate__fadeIn");
+    this.classList.add("hidden");
+  }
+});
+///////////////////////////////// Tabs
+const tabs = document.querySelectorAll(".skills__tab");
+const tabsContainer = document.querySelector(".skills__tab-container");
+const tabsContent = document.querySelectorAll(".skills__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".skills__tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((t) => t.classList.remove("skills__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("skills__content--active"));
+
+  // Activate tab
+  clicked.classList.add("skills__tab--active");
+
+  // Activate content area
+  document
+    .querySelector(`.skills__content--${clicked.dataset.tab}`)
+    .classList.add("skills__content--active");
+});
