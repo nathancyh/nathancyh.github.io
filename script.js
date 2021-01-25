797c35ca7b6738a157022a12299d130fa5267bb3
nathan19952008@gmail.com <nathan19952008@gmail.com>
2021-01-26 01:38:03 +0800

    commit 10

--- script.js
+++ script.js
@@ -7,25 +7,24 @@ ScrollReveal().reveal(".skillrow1", { delay: 250 });
 ScrollReveal().reveal(".work_box", { delay: 250 });
 ScrollReveal().reveal("#contact", { delay: 200 });
 //////////////////////////////NAV SWITCHER
-let switcher = function () {
-  if (nav.classList.contains("navbar-dark")) {
-    nav.classList.remove("navbar-dark");
-    nav.classList.remove("bg-dark");
-    nav.classList.add("navbar-light");
-    nav.classList.add("bg-light");
-  } else {
-    nav.classList.remove("navbar-light");
-    nav.classList.remove("bg-light");
-    nav.classList.add("navbar-dark");
-    nav.classList.add("bg-dark");
-  }
-};
+// let switcher = function () {
+//   if (nav.classList.contains("navbar-dark")) {
+//     nav.classList.remove("navbar-dark");
+//     nav.classList.remove("bg-dark");
+//     nav.classList.add("navbar-light");
+//     nav.classList.add("bg-light");
+//   } else {
+//     nav.classList.remove("navbar-light");
+//     nav.classList.remove("bg-light");
+//     nav.classList.add("navbar-dark");
+//     nav.classList.add("bg-dark");
+//   }
+// };
 
-btnSwit.addEventListener("click", switcher);
+// btnSwit.addEventListener("click", switcher);
 
 //////////////////////////////// STICKY NAV
 const header = document.getElementById("home");
-const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
